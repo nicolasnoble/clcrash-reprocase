54c33de8ced9c502de3310f82d0657ac45e8d351
Nicolas Pixel Noble <pixel@nobis-crew.org>
2019-01-26 19:05:56 -0800

    Repro case

--- main.cc
+++ main.cc
@@ -1,70 +1,39 @@
-#include <cstdio>
+#include <iomanip>
+#include <iostream>
+#include <tuple>
 #include <type_traits>
 #include "typestring.hh"
 
-template<typename A, typename B>
-void check() {
-    printf("[%s] %s\n", std::is_same<A,B>::value ? "pass" : "fail", A::data());
-}
+template <typename type, type defaultValue, typename name>
+class Setting;
+template <typename type, type defaultValue, char... C>
+class Setting<type, defaultValue, irqus::typestring<C...>> {
+  public:
+	type value = defaultValue;
+};
 
-using test1
-    = irqus::typestring<'H', 'e', 'l', 'l', 'o'
-                     , ' '
-                     , 'w', 'o', 'r', 'l', 'd'
-                     , ','
-                     , ' '
-                     , 'h', 'o', 'w'
-                     , ' '
-                     , 'i', 's'
-                     , ' '
-                     , 'i', 't'
-                     , ' '
-                     , 'g', 'o', 'i', 'n', 'g', '?'>;
 
-using test2
-    = irqus::typestring<'u', 'p', 'p', 'e', 'r', 'c', 'a', 's', 'e'
-                       , ':'
-                       , ' '
-                       , 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K'
-                       , 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V'
-                       , 'W', 'X', 'Y', 'Z' >;
-using test3
-    = irqus::typestring< 'l', 'o', 'w', 'e', 'r', 'c', 'a', 's', 'e'
-                       , ':'
-                       , ' '
-                       , 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k'
-                       , 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v'
-                       , 'w', 'x', 'y', 'z' >;
-using test4
-    = irqus::typestring< 'f', 'l','o', 'a', 't', 'i', 'n', 'g'
-                       , ' '
-                       , ':'
-                       , ' '
-                       , '1', '2', '3', '4', '5', '6', '7', '8', '9'
-                       , '.'
-                       , '1', '2', '3', '4', '5', '6', '7', '8', '9'
-                       , '+'
-                       , 'e', '-', '1', '2', '3', '4'>;
-using test5
-    = irqus::typestring< 's', 'y', 'm', 'b', 'o', 'l', 's'
-                       , ' '
-                       , ' '
-                       , ':'
-                       , ' '
-                       , '~', '`', '!', '@', '#', '$', '%', '^', '&', '*', '('
-                       , ')', '_', '+', '=','-', '[', ']', '\\', '{', '}', '|'
-                       , ':', ';', '\'', ',', '.', '?', '/'>;
-      
+typedef typestring_is("Abcdef") Str1;
+typedef typestring_is("Ghijkl") Str2;
+typedef typestring_is("Mnopqr") Str3;
+typedef typestring_is("Stuvwx") Str4;
+typedef typestring_is("Yzabcd") Str5;
+typedef typestring_is("Efghij") Str6;
+typedef typestring_is("Klmnop") Str7;
+typedef typestring_is("Qrstuv") Str8;
+
+typedef Setting<bool, true, Str1> Set1;
+typedef Setting<int, 3, Str2> Set2;
+typedef Setting<bool, true, Str3> Set3;
+typedef Setting<bool, true, Str4> Set4;
+typedef Setting<int, 2, Str5> Set5;
+typedef Setting<int, 2, Str6> Set6;
+typedef Setting<bool, false, Str7> Set7;
+typedef Setting<bool, false, Str8> Set8;
+
+std::tuple<Set1, Set2, Set3, Set4, Set5, Set6, Set7, Set8> container;
+
 int main() {
-    check<typestring_is("Hello world, how is it going?"),test1>();
-    check<typestring_is("uppercase: ABCDEFGHIJKLMNOPQRSTUVWXYZ"),test2>();
-    check<typestring_is("lowercase: abcdefghijklmnopqrstuvwxyz"),test3>();
-    check<typestring_is("floating : 123456789.123456789+e-1234"),test4>();
-    check<typestring_is("symbols  : ~`!@#$%^&*()_+=-[]\\{}|:;',.?/"),test5>();
-    check<typestring_is(""),irqus::typestring<'\0'>>(); // null!
-    check< irqus::tycat< typestring_is("ABC")
-                       , typestring_is("DEF")
-                       , typestring_is("1234")>
-         , typestring_is("ABCDEF1234") >();
-    return {};
+	std::get<Set1>(container).value = false;
+	return 0;
 }
